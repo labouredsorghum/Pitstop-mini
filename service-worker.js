28da33c6eb88f83f13f3b0c94cf7a217a105fe02
labouredsorghum <39.laboured.sorghum@icloud.com>
2025-08-19 14:18:12 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,8 @@
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('psm-v1').then(c=>c.addAll([
-    './','./index.html','./manifest.json'
+  e.waitUntil(caches.open('psp-v1').then(c=>c.addAll([
+    './','./index.html','./manifest.json',
+    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
   ])));
 });
 self.addEventListener('fetch', e=>{
