dcd080ac3217862d291823b30023badcfb09ca87
labouredsorghum <39.laboured.sorghum@icloud.com>
2025-08-19 17:26:18 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,10 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('psp-v3-basic').then(c=>c.addAll(['./','./index.html','./manifest.json'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE='psp-v3-full-1755616088';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json',
+'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+'https://cdn.jsdelivr.net/npm/chart.js',
+'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+'https://html2canvas.hertzen.com/dist/html2canvas.min.js'
+])));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
