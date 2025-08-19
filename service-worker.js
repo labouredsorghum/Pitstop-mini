eeb423f5bc4e0bc236ee0b58f461392e27d620a8
labouredsorghum <39.laboured.sorghum@icloud.com>
2025-08-19 15:40:36 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,11 @@
 self.addEventListener('install', e=>{
- e.waitUntil(caches.open('pspv3').then(c=>c.addAll(['./','./index.html','./manifest.json',
- 'https://cdn.jsdelivr.net/npm/chart.js',
- 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
- 'https://html2canvas.hertzen.com/dist/html2canvas.min.js'
+ e.waitUntil(caches.open('pspv3-admin').then(c=>c.addAll([
+   './','./index.html','./manifest.json',
+   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+   'https://cdn.jsdelivr.net/npm/chart.js',
+   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+   'https://html2canvas.hertzen.com/dist/html2canvas.min.js'
  ])));
 });
 self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
